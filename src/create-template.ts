ee43123b5de4991841c8600f1ecd32546b4a7425
Henrik Agner <henrik.agner@educations.com>
2020-11-06 14:49:48 +0100

    Added custom element decorator

---
+++ src/create-template.ts
@@ -0,0 +1,51 @@
+import { ICustomElementSettings } from "./custom-element.decorator";
+
+export function createTemplate(settings: ICustomElementSettings): HTMLTemplateElement {
+    const html = getHtml(settings.html);
+    const styles = getStyles(settings.styles);
+    const externalStyleSheets = getExternalStyleSheets(settings.externalStyleSheets);
+
+    return createTemplateElement(html, styles, externalStyleSheets);
+}
+
+function createTemplateElement(html: string, style: Partial<CSSStyleDeclaration>, externalStyleSheets: string): HTMLTemplateElement {
+    if (!html) {
+        throw new Error("Could not parse html of component");
+    }
+
+    const template = document.createElement("template");    
+    const innerHTML = `
+        ${externalStyleSheets}
+        ${style}
+        ${html}
+    `
+
+    template.innerHTML = innerHTML;
+    return template;
+}
+
+function getExternalStyleSheets(externalStyleSheets?: Array<string>): string {
+    return externalStyleSheets
+        ? externalStyleSheets.map(styleSheet => `<link rel="stylesheet" href="${styleSheet}">`).join("\n")
+        : "";
+}
+
+function getHtml(html: string): string {
+    let computedHtml: string;
+
+    if (html) {
+        computedHtml = html;
+    }
+
+    return computedHtml ? computedHtml : "";
+}
+
+function getStyles(style: Partial<CSSStyleDeclaration>): Partial<CSSStyleDeclaration> {
+    let computedStyle: Partial<CSSStyleDeclaration>;
+
+    if (style) {
+        computedStyle = style;
+    }
+
+    return computedStyle ? `<style>${computedStyle}</style>` : "";
+}
---
+++ src/custom-element.decorator.ts
@@ -0,0 +1,31 @@
+import { createTemplate } from './create-template';
+
+type Constructor = { new(...args: Array<any>): any };
+
+export function customElement(settings: ICustomElementSettings) {
+    return function <T extends Constructor>(constructorFunction: T) {
+
+        class customElementCreator extends constructorFunction {
+            constructor(...args: Array<any>) {
+                super(args);
+
+                this.attachShadow({ mode: "open" });
+                const template = createTemplate(settings);
+                
+                if (template) {
+                    this.shadowRoot.appendChild(template.content.cloneNode(true));
+                }
+            }
+        }
+
+        customElements.define(settings.name, customElementCreator);
+        return customElementCreator;
+    }
+}
+
+export interface ICustomElementSettings {
+    externalStyleSheets?: Array<string>;
+    html?: string;
+    name: string;
+    styles?: Partial<CSSStyleDeclaration>;
+}
