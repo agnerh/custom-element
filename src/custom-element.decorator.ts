e5109c3a48862a3a56cc9c7b571b4ea5bc73986d
Henrik Agner <henrik.agner@educations.com>
2021-05-11 12:52:28 +0200

    Added check for custom elements before registration

--- src/custom-element.decorator.ts
+++ src/custom-element.decorator.ts
@@ -18,7 +18,7 @@ export function customElement(settings: ICustomElementSettings) {
             }
         }
 
-        if (!customElements.get(settings.name)) {
+        if (customElements && !customElements.get(settings.name)) {
             customElements.define(settings.name, customElementCreator);
         }
         
